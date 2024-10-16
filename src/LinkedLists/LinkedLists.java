a333097445068f22374fca4f3b308c7bebb3ad74
Mihail <mihrdv@gmail.com>
2024-10-16 14:40:33 +0300

    Created a simple linked list from scratch

---
+++ src/LinkedLists/LinkedLists.java
@@ -0,0 +1,102 @@
+package LinkedLists;
+
+class Node {
+    int data;
+    Node next;
+
+    // Constructor
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    private Node head;
+
+    public void insertAtEnd(int data){
+        Node newNode = new Node(data);
+
+        if(head == null){
+            head = newNode;
+        } else {
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void insertAtStart(int data){
+        Node newNode = new Node(data);
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void delete(int data){
+        if(head == null){
+            System.out.println("List is empty");
+            return;
+        } else if (head.data == data) {
+            head = head.next;
+            return;
+        }
+
+        Node current = head;
+        while (current.next != null && current.next.data != data) {
+            current = current.next;  // Move to the next node
+        }
+
+        if (current.next == null) {
+            System.out.println("Element not found");
+        } else {
+            // Remove the node by changing the `next` pointer of the current node
+            current.next = current.next.next;
+        }
+    }
+
+    public void printList() {
+        // If the list is empty
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        // Traverse the list and print each node's data
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");  // Print the current node's data
+            current = current.next;  // Move to the next node
+        }
+        System.out.println("null");  // End of the list
+    }
+}
+
+public class LinkedLists {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        // Insert some elements
+        list.insertAtEnd(10);  // Add 10 at the end
+        list.insertAtEnd(20);  // Add 20 at the end
+        list.insertAtStart(5);  // Add 5 at the beginning
+        list.insertAtEnd(30);  // Add 30 at the end
+
+        // Print the list
+        System.out.println("Current Linked List:");
+        list.printList();  // Prints: 5 -> 10 -> 20 -> 30 -> null
+
+        // Delete an element
+        System.out.println("\nDeleting 20 from the list:");
+        list.delete(20);  // Deletes the node with value 20
+        list.printList();  // Prints: 5 -> 10 -> 30 -> null
+
+        // Try deleting a non-existing element
+        System.out.println("\nTrying to delete 40:");
+        list.delete(40);  // Element 40 doesn't exist in the list
+        list.printList();  // Prints: 5 -> 10 -> 30 -> null
+    }
+
+}
